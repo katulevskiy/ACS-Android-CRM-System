b09f32d54a0b79e87197af030941e55ca43f75f3
Doouble1337 <katulevskiy@gmail.com>
2021-09-03 19:28:38 +0300

    + filled RecyclerView with content + added items into Categories + enabled Scrolling

--- app/src/main/java/com/example/androidcrmsystem/MainActivity.java
+++ app/src/main/java/com/example/androidcrmsystem/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.androidcrmsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.androidcrmsystem.adapter.CategoryAdapter;
+import com.example.androidcrmsystem.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+        categoryList.add(new Category(5, "2005"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+
+        categoryRecycler.setAdapter(categoryAdapter);
+
+
     }
 }
--- app/src/main/java/com/example/androidcrmsystem/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/androidcrmsystem/adapter/CategoryAdapter.java
@@ -1,21 +1,28 @@
 package com.example.androidcrmsystem.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidcrmsystem.R;
 import com.example.androidcrmsystem.model.Category;
 
 import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
 
+///    есть 2 параметра
+///    в первый идет контекст страницы на которой юзер
+///    во второй - список всех категорий
     Context context;
     List<Category> categories;
 
+///    конструктор для установки всех параметров
     public CategoryAdapter(Context context, List<Category> categories) {
         this.context = context;
         this.categories = categories;
@@ -23,12 +30,20 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
 
     @NonNull
     @Override
+
+///    указываем дизайн и элементы, с которыми работаем
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
+        return new CategoryViewHolder(categoryItems);
     }
 
     @Override
+
+    ///   Создаем объект на основе вложенного в него класса
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+    ///   Через объект обращаемся к нужным полям - тексту названий категорий,
+        // И устанавливаем текст для этих категорий из общего списка, созданного в Category.java
+        holder.categoryTitle.setText(categories.get(position).getTitle());
 
     }
 
@@ -37,11 +52,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
         return categories.size();
     }
 
+/// Вложенный класс для onCreateViewHolder для работы с текстовым полем
     public static final class CategoryViewHolder extends RecyclerView.ViewHolder {
 
+        TextView categoryTitle;
 
         public CategoryViewHolder(@NonNull View itemView) {
             super(itemView);
+///           Устанавливаем ссылку на нужный объект из дизайна
+            categoryTitle = itemView.findViewById(R.id.categoryTitle);
         }
 
     }
